4cf197d6ba112a7003ba28d7f2e8eab0cf61ee2c
Joviic <114350371+Joviic@users.noreply.github.com>
2022-09-25 16:23:57 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['2348097206501'] //ur owner number
+global.ownername = "Jovic" //ur owner name
+global.ytname = "YT: Jovic" //ur yt chanel name
+global.socialm = "GitHub: Joviic" //ur github or insta name
+global.location = "Nigeria, Kaduna, Zaria" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.owner = ['2348097206501']
+global.ownertag = '2348097206501' //ur tag number
+global.botname = 'Joviic Bot MD V4' //ur bot name
+global.linkz = "https://wa.me/2348097206501?text=Hi,%20I%20need%20a%20bot" //your theme url which will be displayed on whatsapp
+global.websitex = "https://wa.me/2348097206501?text=Hi,%20I%20need%20a%20bot" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.reactmoji = "🤩" //ur menu react emoji
+global.themeemoji = "🤩" //ur theme emoji
+global.packname = "Jovic Sticker Pack" //ur sticker watermark packname
+global.author = "Jovic" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['2348097206501'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
